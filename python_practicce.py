bfcc28c7e6e0fea6d3b54d9907f2bef4da15bd4a
YeonJae Lee <leeyj1007987@g.skku.edu>
2022-02-15 16:19:49 +0900

    second commit

--- python_practicce.py
+++ python_practicce.py
@@ -1,5 +1,5 @@
 # 예제 1
-'''
+
 print("세 변의 길이를 입력하시오:")
 aa = input()
 bb = input()
@@ -9,10 +9,10 @@ if aa + bb > cc or aa + cc > bb and bb + cc > aa :
   print("yes")
 else :
   print("no")
-'''
+
 
 # 문제 1
-'''
+
 print("원피스의 구매 개수를 입력하시오:")
 onepiece = input()
 print("스웨터의 구매 개수를 입력하시오:")
@@ -29,11 +29,11 @@ elif 9 < (int(onepiece) * 5) + (int(sweater) * 3) < 20 :
 elif 19 < (int(onepiece) * 5) + (int(sweater) * 3) :
   suum = ((int(onepiece) * 5) + (int(sweater) * 3)) * 0.8 
   print("총액:". format(suum * 10000))
-'''
+
 
 # 문제 2 : 일의 자리 수가 3,6,9 인지 여부 확인
 
-'''
+
 print("임의의 정수를 입력하세요:")
 jeongsu = input()
 
@@ -41,10 +41,10 @@ if int(jeongsu) % 10 == 3 or int(jeongsu) % 10 == 6 or int(jeongsu) % 10 == 9 :
   print("yes")
 else :
   print("no")
-'''
+
 
 # 예제 2 : 자릿수의 합
-'''
+
 print("0 ~ 1,000,000 사이의 숫자를 입력하시오:")
 su = input()
 
@@ -59,7 +59,7 @@ suuum = int(aa) + int(bb) + int(cc) + int(dd) + int(ee) + int(ff)
 
 
 print(f"{su}의 자릿수의 합은 {suuum} 입니다.")
-'''
+
 
 # 문제 3 : 2 ** 0 부터 2 ** N 까지 합
 
@@ -80,6 +80,9 @@ bb = int(bb)
 tmp = ("*" * aa) + "\n"
 tmp = tmp * bb
 print(tmp)
+
+
+print(asdfasdfsad)
 
 
 
